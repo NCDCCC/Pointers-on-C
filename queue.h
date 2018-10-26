fb35b9c41de2989419791624291eaa0b0bc7aac6
NCDCCC <ZhangZQ0601@outlook.com>
2018-10-26 17:00:32 +0800

    queue learning

--- queue.h
+++ queue.h
@@ -1,17 +1,42 @@
 #include <stdlib.h>
+#include <assert.h>
 
-#define QUEUE_TYPE     int
+#define   QUEUE_TYPE    int
+#define   QUEUE_SIZE    100
+#define   ARRAY_SIZE    (QUEUE_SIZE + 1)
 
-void create_queue(size_t size);
+static    QUEUE_TYPE   queue[ARRAY_SIZE];
+static    size_t       front = 1;
+static    size_t       rear = 0;
 
-void destroy_queue(void);
+void insert(QUEUE_TYPE value)
+{
+	assert(!is_full());
+	rear = (rear + 1) % ARRAY_SIZE;//loop
+	queue[rear] = value;
+}
 
-void insert( QUEUE_TYPE value);
+QUEUE_TYPE delete(void)
+{
+	QUEUE_TYPE temp;
+	assert(!is_empty());
+	temp = queue[front];
+	front = (front + 1) % ARRAY_SIZE;
+	return temp;
+}
 
-void delete(void);
+QUEUE_TYPE first(void)
+{
+	assert(!is_empty());
+	return queue[front];
+}
 
-QUEUE_TYPE first(void);
+int is_empty(void)
+{
+	return (rear + 1) % ARRAY_SIZE == front;//as 'empty'
+}
 
-int is_empty(void);
-
-int is_full(void);
+int is_full(void)
+{
+	return (rear + 2) % ARRAY_SIZE == front;//as 'full'
+}
